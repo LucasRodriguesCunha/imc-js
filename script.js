d10ecf73570ebf00c1c6cc9211e10be9866c10d4
Lucas Rodrigues Cunha <123827882+LucasRodriguesCunha@users.noreply.github.com>
2023-07-24 20:44:32 -0300

    Finished project

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 const form = document.getElementById("calculadora");
 
 form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    event.preventDefault();
+  const peso = document.getElementById("peso").value;
+  const altura = document.getElementById("altura").value;
 
-    const peso = document.getElementById("peso").value;
-    const altura = document.getElementById("altura").value;
+  const imc = (peso / (altura * altura)).toFixed(2);
 
-    const imc = (peso / (altura * altura)).toFixed(2);
+  const valor = document.getElementById("valor");
+  let descricao = "";
 
-    const valor = document.getElementById("valor");
-    let descricao = "";
+  valor.classList.add("atencao");
 
-    valor.classList.add("atencao");
+  document.getElementById("infos").classList.remove("oculto");
 
-    document.getElementById("infos").classList.remove("oculto");
+  if (imc < 18.5) {
+    descricao = "Cuidado! Você está abaixo do peso!";
+  } else if (imc >= 18 && imc <= 25) {
+    descricao = "Você está no peso ideal!";
+    valor.classList.remove("atencao");
+    valor.classList.add("normal");
+  } else if (imc > 25 && imc <= 30) {
+    descricao = "Cuidado! Você está com sobrepeso!";
+  } else if (imc > 30 && imc <= 35) {
+    descricao = "Cuidado! Você está com obesidade moderada!";
+  } else if (imc > 35 && imc <= 40) {
+    descricao = "Cuidado! Você está com obesidade severa!";
+  } else {
+    descricao = "Cuidado! Você está com obesidade mórbida!";
+  }
 
-    if (imc < 18.5) {
-        descricao = "Cuidado! Você está abaixo do peso!";
-    } else if (imc >= 18 && imc <= 25) {
-        descricao = "Você está no peso ideal!";
-        valor.classList.remove("atencao");
-        valor.classList.add("normal");
-    } else if (imc > 25 && imc <= 30) {
-        descricao = "Cuidado! Você está com sobrepeso!";
-    } else if (imc > 30 && imc <= 35) {
-        descricao = "Cuidado! Você está com obesidade moderada!";
-    } else if (imc > 35 && imc <= 40) {
-        descricao = "Cuidado! Você está com obesidade severa!";
-    } else {
-        descricao = "Cuidado! Você está com obesidade mórbida!";
-    }
-
-    valor.textContent = imc.replace("-", "-");
-    document.getElementById("desc").textContent = descricao;
-
+  valor.textContent = imc.replace("-", "-");
+  document.getElementById("desc").textContent = descricao;
 });
